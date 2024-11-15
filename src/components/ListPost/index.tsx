7c7beb63dc78343b3b60e769b060ae52668ea774
João Dias <joao.dias1994@hotmail.com>
2024-11-14 23:32:09 -0300

    <CHORE>adicionando propriedade embed ao fetch dos posts

--- src/components/ListPost/index.tsx
+++ src/components/ListPost/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default async function ListPost() {
-    const blogPosts =  await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/wp-json/wp/v2/posts?per_page=3&order=desc&status=publish`)
+    const blogPosts =  await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/wp-json/wp/v2/posts?_embed&per_page=3&order=desc&status=publish`)
     const posts = await blogPosts.json();
     const options = {
         day: 'numeric',
@@ -18,7 +18,7 @@ export default async function ListPost() {
                     <p className="max-w-screen-md text-left text-gray-500 md:text-base sub-title">Abaixo nossos posts mais recentes, e não esquece de se inscrever na newsletter para ficar por dentro de todas as novidades!</p>
                 </div>
 
-                {/* <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-3 xl:gap-8">
+                <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-3 xl:gap-8">
                     {posts.map(post => (
                         <div key={post.id} className="group relative flex h-48 flex-col overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-64 xl:h-96">
                             <Link href="#" ></Link>
@@ -36,7 +36,7 @@ export default async function ListPost() {
                             </div>
                         </div>
                     ))}
-                </div> */}
+                </div>
             </div>
         </div>
     )
